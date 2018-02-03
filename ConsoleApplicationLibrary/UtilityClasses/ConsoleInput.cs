242f19d0859eff3f62b733818f85aba80ea67412
evgen10 <i.am.mr.jackson96@gmail.com>
2018-02-03 17:13:02 +0600

    one

---
+++ ConsoleApplicationLibrary/UtilityClasses/ConsoleInput.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationLibrary.UtilityClasses
+{
+
+    /// <summary>
+    /// Предоставляет методы для корректного ввода данных
+    /// </summary>
+    public class ConsoleInput
+    {
+        /// <summary>
+        /// Метод обеспечивает верный ввод целого положительного числа
+        /// </summary>
+        /// <returns>Целое положительное число</returns>
+        public static int EnterPositiveIntengerNumber()
+        {
+            //цикл выполняется пока не будет введено целое положительное число
+            while (true)
+            {
+                int number;
+                Console.Write("Введите целое положительное число: ");
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    if (number >= 0)
+                    {
+                        return number;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Вы ввели отрицательное число: ");
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Неверные данные");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Метод обеспечивает верный ввод целого числа
+        /// </summary>
+        /// <returns>Целое число</returns>
+        public static int EnterIntengerNumber()
+        {
+            while (true)
+            {
+                int number;
+                Console.Write("Введите целое число: ");
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    return number;
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Неверные данные");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// Метод обеспечивает верный ввод вещественного положительного числа
+        /// </summary>
+        /// <returns>Целое число</returns>
+        public static double EnterPositiveRealNumber()
+        {
+            while (true)
+            {
+                double number;
+                Console.Write("Введите положительное число: ");
+                if (double.TryParse(Console.ReadLine(), out number))
+                {
+                    if (number >= 0)
+                    {
+                        return number;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Вы ввели отрицательное число: ");
+                        Console.ResetColor();
+                    }
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Неверные данные");
+                    Console.ResetColor();
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApplicationLibrary/UtilityClasses/ProgramExit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationLibrary.UtilityClasses
+{
+
+    /// <summary>
+    /// Содержит методы для выхода из программы.
+    /// </summary
+    public class ProgramExit
+    {
+        /// <summary>
+        /// Метод возвращает false если нажата любая клавиша кроме "ESC"
+        /// </summary>
+        /// <returns></returns>
+        public static bool Exit()
+        {
+            Console.WriteLine("Для продолжения нажмите любую клавишу. Для выхода \"ESC\"\n");
+            if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+    }
+}
